c82c33ca1acee4ee70bd8d49b45d5c0077fb2166
Thiago Elias <thiagoelias99@gmail.com>
2025-05-06 18:31:31 -0300

    set app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from "@nestjs/common"
+import { useContainer } from "class-validator"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('v1')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+  await app.listen(process.env.PORT ?? 3000)
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
-bootstrap();
+bootstrap()
